0c4ad4ba7c27b615f33d4f4bc17ded5ae699d871
Dharmendeer <saferzone56@gmail.com>
2023-08-20 15:40:49 -0400

    add massege in add.py

--- add.py
+++ add.py
@@ -1,3 +1,4 @@
 a =10
 b =20
 print("Sum of two numbers are : ",a+b)
+# Dharmender
---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum{
+
+
+    public static void maiin(string[] args){
+        int a=5;
+        int b=6;
+        System.out.println(a+b);
+    }
+}
